84c9ce3eb89f7db5ab3c0a2e5426d426c3bde6d4
Colin Lambert <clambertcello@gmail.com>
2022-06-12 14:54:29 -0700

    Removed unused vars

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Nav, Navbar, NavItem } from 'reactstrap';
 
 const Header = () => {
