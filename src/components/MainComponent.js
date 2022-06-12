5772997d960f6e407f0741e93ed361412da5545c
Colin Lambert <clambertcello@gmail.com>
2022-06-12 12:28:53 -0700

    Header and About Shells

---
+++ src/components/MainComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './HeaderComponent';
+import About from './AboutComponent';
+
+const Main = () => {
+    return (
+        <div>
+            <Header />
+            <About />
+        </div>
+    )
+}
+
+export default Main;
