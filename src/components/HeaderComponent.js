c38517c69b18192386f4498613e841612a9e3991
Colin Lambert <clambertcello@gmail.com>
2022-06-12 13:12:21 -0700

    Set up initial routing

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Nav, Navbar, NavItem } from 'reactstrap';
 
 const Header = () => {
     return (
         <div>
             <h1>This is the header!</h1>
+            <NavList />
         </div>
+    );
+}
+
+const NavList = () => {
+    return (
+        <Navbar dark sticky='top'>
+            <div className='container'>
+                <Nav navbar>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/about'>About</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/projects'>Projects</NavLink>
+                    </NavItem>
+                </Nav>
+            </div>
+        </Navbar>
     );
 }
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Header from './HeaderComponent';
 import About from './AboutComponent';
+import Projects from './ProjectsComponent';
+import { Route, Routes } from 'react-router-dom';
 
 const Main = () => {
     return (
         <div>
             <Header />
-            <About />
+            <Routes>
+                <Route path='/about' element={<About />} />
+                <Route path='/projects' element={<Projects />} />
+            </Routes>
+
         </div>
     )
 }
