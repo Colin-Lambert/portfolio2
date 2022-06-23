d40de5838cf991aa480d9df2fd7a076d469936c2
Colin-Lambert <colin.lambert.dev@gmail.com>
2022-06-23 14:03:17 -0700

    Placeholder for project displays

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
+import { baseUrl } from "../shared/baseUrl";
+import { PROJECTS } from "../shared/projects";
 
 const Projects = () => {
     return (
-        <div>These are my projects!</div>
+        <div>
+            <h2>These are my projects!</h2>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <p>Hopefully my projects will be here soon.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
+}
+
+class RenderProjects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
 }
 
 export default Projects;
