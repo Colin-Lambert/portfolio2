4d7920684181c1ca54d4ab3fe3bfaa48db7d937d
Colin-Lambert <colin.lambert.dev@gmail.com>
2022-06-28 16:19:57 -0700

    Horizontal navbar and empty toggle button

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Nav, Navbar, NavItem } from 'reactstrap';
+import { Collapse, Nav, Navbar, NavbarBrand, NavItem, NavbarToggler } from 'reactstrap';
 import headshot from '../images/cello-headshot.JPG';
 
 const Header = () => {
@@ -8,22 +8,39 @@ const Header = () => {
         <div className='container'>
             <div className='row'>
                 <div className='col-md-2 mt-auto'>
-                    <img src={headshot} height='120' width='100' alt='headshot' className='headshot d-md-inline-flex' />
+                    <img src={headshot} height='120' width='100' alt='headshot' className='headshot' />
                 </div>
                 <div className='col-md'>
-                    <h1>colin.lambert</h1>
+                    <h1>colin.lambert/front-end-developer</h1>
                 </div>
             </div>
-            <NavList />
+            <div className='row'>
+                <NavList />
+            </div>
         </div>
     );
 }
 
 const NavList = () => {
     return (
-        <Navbar dark sticky='top' className='mr-auto'>
-            <div className='container'>
-                <Nav navbar>
+        <Navbar
+            color='dark'
+            dark
+            sticky='top'
+            expand='md'
+        >
+            <NavbarBrand href='/'>
+                CL
+            </NavbarBrand>
+            <NavbarToggler onClick={function noRefCheck() { }} />
+            <Collapse
+                isOpen
+                navbar
+            >
+                <Nav
+                    className='me-auto'
+                    navbar
+                >
                     <NavItem>
                         <NavLink className='nav-link' to='/about'>About</NavLink>
                     </NavItem>
@@ -31,7 +48,7 @@ const NavList = () => {
                         <NavLink className='nav-link' to='/projects'>Projects</NavLink>
                     </NavItem>
                 </Nav>
-            </div>
+            </Collapse>
         </Navbar>
     );
 }
