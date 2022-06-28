04616deb128377f95e02cd259a5774729e064ec4
Colin-Lambert <colin.lambert.dev@gmail.com>
2022-06-28 16:30:51 -0700

    NavbarToggler functional

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Collapse, Nav, Navbar, NavbarBrand, NavItem, NavbarToggler } from 'reactstrap';
 import headshot from '../images/cello-headshot.JPG';
@@ -21,36 +21,52 @@ const Header = () => {
     );
 }
 
-const NavList = () => {
-    return (
-        <Navbar
-            color='dark'
-            dark
-            sticky='top'
-            expand='md'
-        >
-            <NavbarBrand href='/'>
-                CL
-            </NavbarBrand>
-            <NavbarToggler onClick={function noRefCheck() { }} />
-            <Collapse
-                isOpen
-                navbar
+class NavList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
+    render() {
+        return (
+            <Navbar
+                color='dark'
+                dark
+                sticky='top'
+                expand='md'
             >
-                <Nav
-                    className='me-auto'
+                <NavbarBrand href='/'>
+                    CL
+                </NavbarBrand>
+                <NavbarToggler onClick={this.toggleNav} />
+                <Collapse
+                    isOpen={this.state.isNavOpen}
                     navbar
                 >
-                    <NavItem>
-                        <NavLink className='nav-link' to='/about'>About</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink className='nav-link' to='/projects'>Projects</NavLink>
-                    </NavItem>
-                </Nav>
-            </Collapse>
-        </Navbar>
-    );
+                    <Nav
+                        className='me-auto'
+                        navbar
+                    >
+                        <NavItem>
+                            <NavLink className='nav-link' to='/about'>About</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className='nav-link' to='/projects'>Projects</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        );
+    }
 }
 
 export default Header;
