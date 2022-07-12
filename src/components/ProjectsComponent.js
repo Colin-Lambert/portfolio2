86dd368e0d7a31a91e9b32f9a7223171a16e144a
Colin-Lambert <colin.lambert.dev@gmail.com>
2022-07-12 15:06:59 -0700

    Put ProjectsList in Container

--- src/components/ProjectsComponent.js
+++ src/components/ProjectsComponent.js
@@ -1,25 +1,27 @@
 import React from "react";
-import { Col, Row } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 import ProjectCard from "./ProjectCardComponent";
 
 const ProjectsList = (props) => {
     const projects = props.projects;
 
     return (
-        <Row className="ms-auto">
-            {projects.map((project) => {
-                return (
-                    <Col
-                        md='5'
-                        className="m-4 xyz-in"
-                        xyz='fade up right rotate-right duration-10 ease-out-back stagger'
-                        key={project.id}
-                    >
-                        <ProjectCard project={project} />
-                    </Col>
-                );
-            })}
-        </Row>
+        <Container>
+            <Row className="ms-auto">
+                {projects.map((project) => {
+                    return (
+                        <Col
+                            md='5'
+                            className="m-4 xyz-in"
+                            xyz='fade up right rotate-right duration-10 ease-out-back stagger'
+                            key={project.id}
+                        >
+                            <ProjectCard project={project} />
+                        </Col>
+                    );
+                })}
+            </Row>
+        </Container>
     );
 }
 
